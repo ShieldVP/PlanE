bde87cde6ad6c7e3c8748b4cee87c3a0a2eefe7c
rainsmaker4848 <69397918+rainsmaker4848@users.noreply.github.com>
2020-08-16 16:43:47 +0300

    Add files via upload

---
+++ people_on_theme.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import os
+def min_people(name, mint): 
+    p1 = pd.read_excel(os.path.join(r'input_data', 'amount_of_days.xlsx'))
+    sotr = p1[name][2]
+    days_for_one_team = p1[name][0] 
+    program_days = p1[name][1] #в последствии, из разных файлов
+    howmuch = mint * program_days / days_for_one_team - sotr
+    return (howmuch, program_days / days_for_one_team)
