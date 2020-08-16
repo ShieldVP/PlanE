0c11f80748aa9c470e759be182a7cb882ccc918c
rainsmaker4848 <69397918+rainsmaker4848@users.noreply.github.com>
2020-08-16 18:42:47 +0300

    Add files via upload

--- external_students.py
+++ external_students.py
@@ -3,10 +3,15 @@ import minimal_count_of_people_in_group as one
 import people_on_theme as two
 import os
 def how_much_man(list1):
+    dict2 = dict()  
     i=0
     m = len(list1)
     howmuchpeople = 0
     for i in range(m):
         a = one.min_band(list1[i])
-        howmuchpeople = howmuchpeople + two.min_people(list1[i], a)
-    return (howmuchpeople)
+        e,f = two.min_people(list1[i], a)
+        howmuchpeople = howmuchpeople + e
+        dict1 = {list1[i]: [f, e]}
+        dict2.update(dict1)
+        
+    return howmuchpeople, dict2
