c5384419cc27f577218cdaf34c15f78ce8874087
rainsmaker4848 <69397918+rainsmaker4848@users.noreply.github.com>
2020-08-16 14:02:07 +0300

    Add files via upload

---
+++ external_students.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import mint as one
+import raspred as two
+import os
+def how_much_man(list1):
+    i=0
+    m = len(list1)
+    howmuchpeople = 0
+    for i in range(m):
+        a = one.min_band(list1[i])
+        howmuchpeople = howmuchpeople + two.min_people(list1[i], a)
+    return (howmuchpeople)
